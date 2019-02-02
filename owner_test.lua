8ccb2666c4c9885e0bbdc5cd1ac196a575998e4f
Hmm465 <29178963+Hmm465@users.noreply.github.com>
2019-02-02 15:41:25 -0500

    Update owner_test.lua

--- owner_test.lua
+++ owner_test.lua
@@ -1,6 +1,6 @@
 if owner == nil then
 print("detected that owner isnt supported, using something else instead")
-owner = game:GetService("Players"):FindFirstChild("YourName")
+owner = game:GetService("Players"):FindFirstChild("scrIptk1itty")
 end
 
 print(""..owner.Name)
